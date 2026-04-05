87d63b72afa331d19d9fee67bc05d2cf2d774ec1
Ram Parnam <ramu.parnam@gmail.com>
2026-04-06 00:09:39 +0530

    second run

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "your CICD application is successfully!!!, you have achieved this!"
+    return "your CICD application is successfully!!!, this is the second run"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)   
